e71effa145f8fddba95022cfb3370405eb1e6b36
Carl Otto <33408758+Distortions81@users.noreply.github.com>
2025-08-25 08:50:44 -0600

    Add dice roll and weapon cycle plugin examples

---
+++ example_plugins/weapon_cycle.go
@@ -0,0 +1,44 @@
+//go:build plugin
+
+package main
+
+import (
+	"strings"
+
+	"gt"
+)
+
+// PluginName identifies this plugin in the UI.
+var PluginName = "Weapon Cycle"
+
+var cycleItems = []string{"Axe", "Short Sword", "Dagger", "Chocolate"}
+
+// Init binds F3 to cycle through weapons.
+func Init() {
+	gt.RegisterFunc("cycleWeapon", cycleWeapon)
+	gt.AddHotkeyFunc("F3", "cycleWeapon")
+}
+
+func cycleWeapon() {
+	inv := gt.Inventory()
+	current := ""
+	for _, it := range inv {
+		if it.Equipped {
+			current = it.Name
+			break
+		}
+	}
+	next := cycleItems[0]
+	for i, name := range cycleItems {
+		if strings.EqualFold(current, name) {
+			next = cycleItems[(i+1)%len(cycleItems)]
+			break
+		}
+	}
+	for _, it := range inv {
+		if strings.EqualFold(it.Name, next) {
+			gt.Equip(it.ID)
+			return
+		}
+	}
+}
