c95573ed3a21b816be6366ade6ab625ce95d5394
Distortions81 <carlotto81@gmail.com>
2025-08-26 15:31:41 -0600

    .

---
+++ example_plugins/keep_alive.go
@@ -0,0 +1,39 @@
+package plugins
+
+import (
+	"gt"
+	"time"
+)
+
+const maxKeepAlive = 5
+
+var (
+	PluginName     = "keep_alive"
+	keepAliveCount = 0
+	lastKeepalive  time.Time
+)
+
+func init() {
+	gt.RegisterChatHandler(watchChat)
+	lastKeepalive = time.Now()
+}
+
+func watchChat(msg string) {
+	if msg == "Please do something to avoid being disconnected." {
+
+		if time.Since(lastKeepalive) < time.Minute*2 {
+			//Way too soon
+			return
+		} else if time.Since(lastKeepalive) > time.Minute*30 {
+			//Its been long enough.. User is not AFK reset the count
+			keepAliveCount = 0
+		}
+		if keepAliveCount > maxKeepAlive {
+			//Don't prevent disconnect forever
+			return
+		}
+		gt.EnqueueCommand("/money")
+		lastKeepalive = time.Now()
+		keepAliveCount++
+	}
+}
